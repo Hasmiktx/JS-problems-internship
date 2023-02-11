9cced0dfda532e61e835d762c941597e5a01a086
Hasmiktx <hasmiktx@gmail.com>
2023-02-11 22:55:54 +0400

    iterative version

---
+++ Algorithms/binarySearch.js
@@ -0,0 +1,15 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function binarySearch(arr, target, left = 0, right = arr.length - 1) {
+  while (right >= left) {
+    let mid = Math.floor((left + right) / 2);
+    if (target === arr[mid]) {
+      return mid;
+    } else if (target > arr[mid]) {
+      left = mid + 1;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(array, 9));
