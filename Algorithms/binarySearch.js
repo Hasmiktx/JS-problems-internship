a41b09021210e81e83d9a92fa3dd0138a9ecd1b6
Hasmiktx <hasmiktx@gmail.com>
2023-02-11 23:25:51 +0400

    recursive solution

--- Algorithms/binarySearch.js
+++ Algorithms/binarySearch.js
@@ -1,4 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Iterative solution
 function binarySearch(arr, target, left = 0, right = arr.length - 1) {
   while (right >= left) {
     let mid = Math.floor((left + right) / 2);
@@ -12,4 +14,19 @@ function binarySearch(arr, target, left = 0, right = arr.length - 1) {
   }
   return -1;
 }
-console.log(binarySearch(array, 9));
+// Recursive solution
+function binarySearch(arr, target, left = 0, right = arr.length - 1) {
+  if (right >= left) {
+    let mid = Math.floor((left + right) / 2);
+    if (target === arr[mid]) {
+      return mid;
+    } else if (target > arr[mid]) {
+      return binarySearch(arr, target, mid + 1, right);
+    } else if (target < arr[mid]) {
+      return binarySearch(arr, target, left, mid - 1);
+    }
+  } else {
+    return -1;
+  }
+}
+console.log(binarySearch(array, 2));
